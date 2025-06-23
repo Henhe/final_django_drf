9d2583ff3b68acb1f7f7333bdb552c9e838cfd4d
Henhe <Henhe@Rambler.ru>
2025-06-24 00:36:09 +0300

    test

--- final_project/author/tests.py
+++ final_project/author/tests.py
@@ -1,3 +1,258 @@
 from django.test import TestCase
+from author.models import Author, Country
+from user.models import User, UserRole
+import random
+import json
+from unittest import mock
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class CountryTestCaseAnon(TestCase):
+    def setUp(self) -> None:
+        user = User.objects.create_user(
+            "test@test.com",
+            "test",
+            is_active=True,
+            role=UserRole.ADMIN.value
+        )
+        user.set_password("password")
+        user.save()
+        country1 = Country.objects.create(id=1, name="США", creator=user)
+        country2 = Country.objects.create(id=2, name="РФ", creator=user)
+
+    def test_post_country_anon(self):
+        country_data = {
+            "name": "test",
+            "creator": 1,
+        }
+        response = self.client.post("/api/country/")
+        self.assertEqual(response.status_code, 401)
+
+    def test_auth_user(self):
+        user_data = {
+            "username": "test",
+            "password": "password",
+        }
+        response = self.client.post("/api/user/token-auth/", data=user_data)
+        data = json.loads(response.content)
+        user = User.objects.filter(username=user_data["username"]).first()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["data"]["token"], token.key)
+
+    def test_get_countries_list(self):
+        response = self.client.get("/api/country/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_country(self):
+        response = self.client.get("/api/country/1/")
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertEqual(data["name"], "США")
+
+    def test_object_creation(self):
+        self.assertEqual(Country.objects.count(), 2)
+
+
+class CountryTestCase(TestCase):
+    def setUp(self) -> None:
+        user = User.objects.create_user(
+            "test@test.com",
+            "test",
+            is_active=True,
+            role=UserRole.ADMIN.value
+        )
+        user.set_password("password")
+        user.save()
+        country1 = Country.objects.create(id=1, name="США", creator=user)
+        country2 = Country.objects.create(id=2, name="РФ", creator=user)
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.defaults["HTTP_AUTHORIZATION"] = "Token " + token.key
+
+    def test_post_delete_country(self):
+        user = User.objects.filter(username="test").first()
+        token, _ = Token.objects.get_or_create(user=user)
+
+        country_data = {
+            "name" : "test11",
+            "id" :3
+        }
+        data = json.dumps(country_data)
+        response = self.client.post("/api/country/",
+                                    data=data,
+                                    HTTP_AUTHORIZATION=f"Token {token}",
+                                    content_type="application/json")
+        data = json.loads(response.content)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data["name"], "test11")
+
+        # country_data = {
+        #     "id": 3
+        # }
+        # data = json.dumps(country_data)
+        # response = self.client.delete("/api/country/",
+        #                             data=data,
+        #                             HTTP_AUTHORIZATION=f"Token {token}",
+        #                             content_type="application/json")
+        # # response = self.client.request("DELETE", "/api/country/", json=data, HTTP_AUTHORIZATION=f"Token {token}")
+        # data = json.loads(response.content)
+        # print(f'{data=}')
+        # print(f'{response.status_code=}')
+
+    def test_patch_country(self):
+        user = User.objects.filter(username="test").first()
+        token, _ = Token.objects.get_or_create(user=user)
+
+        country_data = {
+            "id": 2,
+            "name" : "test_new",
+            "creator": 1
+        }
+        data = json.dumps(country_data)
+        response = self.client.patch("/api/country/2/",
+                                    data=data,
+                                    HTTP_AUTHORIZATION=f"Token {token}",
+                                    content_type="application/json")
+        data = json.loads(response.content)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data["name"], "test_new")
+
+# class AnonUserTestCase(TestCase):
+#     def setUp(self) -> None:
+#         user = User.objects.create_user(
+#             "test@test.com",
+#             "test",
+#             is_active=True,
+#             first_name="John",
+#             role=UserRole.ADMIN.value
+#         )
+#         user.set_password("password")
+#         user.save()
+#
+#     def test_get_users_list_as_anon(self):
+#         response = self.client.get("/api/users/")
+#         self.assertEqual(response.status_code, 401)
+#
+#     @mock.patch("user.service.UserService.send_confirmation_email")
+#     def test_register_user(self, mock_send_email):
+#         user_data = {
+#             "email": "test_reg@email.com",
+#             "username": "new_test",
+#             "password": "hardpassword@@123",
+#         }
+#         response = self.client.post("/api/register/", data=user_data)
+#         data = json.loads(response.content)
+#         new_user = User.objects.filter(username=user_data["username"]).first()
+#         self.assertEqual(response.status_code, 201)
+#         self.assertEqual(data["email"], user_data["email"])
+#         self.assertEqual(data["username"], user_data["username"])
+#         self.assertEqual(data["isActive"], False)
+#         mock_send_email.assert_called()
+#         mock_send_email.assert_called_with(new_user)
+#
+#     def test_auth_user(self):
+#         user_data = {
+#             "username": "test",
+#             "password": "password",
+#         }
+#         response = self.client.post("/api/token-auth/", data=user_data)
+#         data = json.loads(response.content)
+#         user = User.objects.filter(username=user_data["username"]).first()
+#         token, _ = Token.objects.get_or_create(user=user)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(data["token"], token.key)
+#
+#     @mock.patch("user.service.UserService.activate_user")
+#     def test_activate_user(self, activate_user_mock):
+#         response = self.client.post("/api/activate/?uuid=asd&token=aasdqwe123")
+#         data = json.loads(response.content)
+#         # user = User.objects.filter(username=user_data["username"]).first()
+#         # token, _ = Token.objects.get_or_create(user=user)
+#         self.assertEqual(response.status_code, 200)
+#         activate_user_mock.assert_called_with("asd", "aasdqwe123")
+#
+#
+# class UserTestCase(TestCase):
+#     # def create_test_users(self, count):
+#     #     print("Setup users")
+#     #     for number in range(count):
+#     #         user = User.objects.create_user(
+#     #             f"test{number}@test.com",
+#     #             f"test{number}",
+#     #             is_active=random.choice([True, False]),
+#     #             role=random.choice(UserRole.values())
+#     #         )
+#     #         user.set_password("password")
+#     #         user.save()
+#
+#     def setUp(self) -> None:
+#         user = User.objects.create_user(
+#             "test@test.com",
+#             "test",
+#             is_active=True,
+#             first_name="John",
+#             role=UserRole.ADMIN.value
+#         )
+#         user.set_password("password")
+#         user.save()
+#         token, _ = Token.objects.get_or_create(user=user)
+#         self.client.defaults["HTTP_AUTHORIZATION"] = "Token " + token.key
+#         for number in range(5):
+#             user = User.objects.create_user(
+#                 f"test{number}@test.com",
+#                 f"test{number}",
+#                 is_active=random.choice([True, False]),
+#                 role=random.choice(UserRole.values())
+#             )
+#             user.set_password("password")
+#             user.save()
+#
+#     def test_get_users_list_order_by_id(self):
+#         first_user = User.objects.order_by("id").first()
+#         response = self.client.get("/api/users/?ordering=id")
+#         data = json.loads(response.content)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(len(data["data"]), 1)
+#         self.assertEqual(data["meta"]["count"], 6)
+#         self.assertEqual(data["meta"]["page"], 1)
+#         self.assertEqual(data["meta"]["perPage"], 1)
+#         self.assertEqual(data["data"][0]["id"], first_user.id)
+#
+#     def test_get_users_list(self):
+#         last_user = User.objects.order_by("-id").first()
+#         response = self.client.get("/api/users/")
+#         data = json.loads(response.content)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(len(data["data"]), 1)
+#         self.assertEqual(data["meta"]["count"], 6)
+#         self.assertEqual(data["meta"]["page"], 1)
+#         self.assertEqual(data["meta"]["perPage"], 1)
+#         self.assertEqual(data["data"][0]["id"], last_user.id)
+#
+#     def test_get_users_list_paginated(self):
+#         response = self.client.get("/api/users/?page=2&perPage=3")
+#         data = json.loads(response.content)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(len(data["data"]), 3)
+#         self.assertEqual(data["meta"]["count"], 6)
+#         self.assertEqual(data["meta"]["page"], 2)
+#         self.assertEqual(data["meta"]["perPage"], 3)
+#
+#     def test_get_users_list_with_search(self):
+#         response = self.client.get("/api/users/?search=jo")
+#         data = json.loads(response.content)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(len(data["data"]), 1)
+#         self.assertEqual(data["meta"]["count"], 1)
+#         self.assertEqual(data["meta"]["page"], 1)
+#         self.assertEqual(data["meta"]["perPage"], 1)
+#         self.assertEqual(data["data"][0]["firstName"], "John")
+#
+#     def test_get_users_list_with_role_filter(self):
+#         admin_count = User.objects.filter(role=UserRole.ADMIN.value).count()
+#         response = self.client.get(f"/api/users/?role={UserRole.ADMIN.value}&perPage=10")
+#         data = json.loads(response.content)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(len(data["data"]), admin_count)
+#         self.assertEqual(data["meta"]["count"], admin_count)
+#         self.assertEqual(data["meta"]["page"], 1)
+#         self.assertEqual(data["meta"]["perPage"], 10)
